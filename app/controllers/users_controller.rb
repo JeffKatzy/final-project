5376356e08bf817a1564b9fd7aacd0b1251d9c47
nikymorg <nicole.morgan@gmail.com>
2016-12-01 11:24:21 -0500

    so broken

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  skip_before_action :authenticate_user, :only => [:new, :create]
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def show
+    user = User.find(params[:id])
+    jwt = Auth.issue({user_id: user.id})
+    render json: {jwt: jwt}
+  end
+
+  def create
+    byebug
+    user = User.new(user_params)
+    if user.save
+      jwt = Auth.issue({user_id: user.id})
+      render json: {jwt: jwt}
+    else
+      render json: {error: 'user not unique'}
+    end
+  end
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+end
