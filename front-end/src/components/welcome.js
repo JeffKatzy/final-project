3915f9a7d7f3aa6d1b865ec789d71661cb9d50e8
cspenc <crs992@gmail.com>
2016-12-01 14:45:56 -0500

    janky code

--- front-end/src/components/welcome.js
+++ front-end/src/components/welcome.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import SignIn from './signin'
 import SignUp from './signup'
+// import { Link } from 'react-router'
 
 
 class Welcome extends React.Component {
@@ -10,25 +11,24 @@ class Welcome extends React.Component {
 
     this.handleSignIn = this.handleSignIn.bind(this)
     this.handleSignUp = this.handleSignUp.bind(this)
+    this.state = {showSignIn: false}
   }
 
   handleSignIn(){
     console.log("YES")
-    return(<SignIn />)
+    this.setState({showSignIn: true})
   }
 
   handleSignUp(){
     console.log("NO")
-
-    render(){
-      return(<SignUp />)
-    }
+    return (<SignUp />)
   }
 
   render(){
     return(
       <div>
         <button onClick={this.handleSignIn}>Sign In</button>
+        {this.state.showSignIn ? <SignIn /> : null}
         <button onClick={this.handleSignUp}>Sign Up</button>
       </div>
     )
