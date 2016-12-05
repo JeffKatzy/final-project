b225cb0d127350de8c7efdef9f5b33582522fe58
nikymorg <nicole.morgan@gmail.com>
2016-12-05 12:03:11 -0500

    Katzy edits

--- front-end/src/components/users.js
+++ front-end/src/components/users.js
@@ -1,20 +1,19 @@
-// import React, { Component } from 'react';
-// import { connect } from 'react-redux';
-//
-// class Users extends Component {
-//   render(){
-//     let users = this.props.users.map(function(user){
-//       return <li> {user.id} {user.name} </li>
-//     })
-//     return(<div>
-//       {users}
-//       </div>)
-//   }
-// }
-//
-// function mapStateToProps(state){
-//   return {users: state.users }
-// }
-//
-//
-// export default connect(mapStateToProps)(Users)
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Users extends Component {
+  render(){
+    let users = this.props.users.map(function(user){
+      return <li> {user.id} {user.name} </li>
+    })
+    return(<div>
+      {users}
+      </div>)
+  }
+}
+
+function mapStateToProps(state){
+  return {users: state.users }
+}
+
+export default connect(mapStateToProps)(Users)
